c9242d928402b236228e02a3fc194ccb48f6f66c
makeinjava <makeinjava@gmail.com>
2016-07-20 22:10:43 +0530

    Update binary tree structure

--- Quasi-IsomorphicBinaryTrees(DFS)/src/main/java/org/learn/Client/App.java
+++ Quasi-IsomorphicBinaryTrees(DFS)/src/main/java/org/learn/Client/App.java
@@ -8,43 +8,43 @@ public class App
 	public static void main( String[] args )
     {		
 		//level 0
-		Node tree1 = Node.createNode(100); //A
+		Node tree1 = Node.createNode(45); //A
 		//level 1
 		tree1.left = Node.createNode(20); //B
-		tree1.right = Node.createNode(115);//C
+		tree1.right = Node.createNode(65);//C
 		// left sub tree
-		tree1.left.left = Node.createNode(170);//D
-		tree1.left.left.left = Node.createNode(140);//G
-		tree1.left.left.right = Node.createNode(139); //H
+		tree1.left.left = Node.createNode(49);//D
+		tree1.left.left.left = Node.createNode(30);//G
+		tree1.left.left.right = Node.createNode(45); //H
 
 		// right subtree
 		tree1.right.left = Node.createNode(30); //E
-		tree1.right.right = Node.createNode(40);//F
+		tree1.right.right = Node.createNode(60);//F
 		
-		tree1.right.right.left = Node.createNode(40);//I	
-		tree1.right.right.right = Node.createNode(40);//J	
+		tree1.right.right.left = Node.createNode(65);//I	
+		tree1.right.right.right = Node.createNode(55);//J	
 		
 		
-		Node tree2 = Node.createNode(400); //A
+		Node tree2 = Node.createNode(70); //P
 		//level 1
-		tree2.left = Node.createNode(100); //B
-		tree2.right = Node.createNode(90);//C
+		tree2.left = Node.createNode(55); //Q
+		tree2.right = Node.createNode(85);//R
 		// left sub tree
-		tree2.left.left = Node.createNode(25);//D
-		tree2.left.left.left = Node.createNode(120);//G
-		tree2.left.left.right = Node.createNode(200); //H
+		tree2.left.left = Node.createNode(30);//S
+		tree2.left.left.left = Node.createNode(80);//T
+		tree2.left.left.right = Node.createNode(30); //U
 
 		// right subtree
-		tree2.right.left = Node.createNode(30); //F
-		tree2.right.right = Node.createNode(40); //E
-		tree2.right.left.left = Node.createNode(40); //2
-		tree2.right.left.right = Node.createNode(40); //2
+		tree2.right.left = Node.createNode(40); //V
+		tree2.right.right = Node.createNode(65); //W
+		tree2.right.left.left = Node.createNode(52); //X
+		tree2.right.left.right = Node.createNode(10); //Y
 
 		boolean isSame = QuasiIsomorphicBinaryTree.quasiIsomorphicBinaryTree(tree1, tree2);
 		if(isSame) {
-			System.out.println("Binary Trees are Quasi-Isomorphic");
+			System.out.println("Binary trees are Quasi-Isomorphic");
 		} else {
-			System.out.println("Binary Trees are not Quasi-Isomorphic");
+			System.out.println("Binary trees are not Quasi-Isomorphic");
 		}
     }
 }
--- Quasi-IsomorphicBinaryTrees(DFS)/src/main/java/org/learn/Question/QuasiIsomorphicBinaryTree.java
+++ Quasi-IsomorphicBinaryTrees(DFS)/src/main/java/org/learn/Question/QuasiIsomorphicBinaryTree.java
@@ -1,18 +1,21 @@
-
 package org.learn.Question;
 
 public class QuasiIsomorphicBinaryTree {
+	
 	public static boolean quasiIsomorphicBinaryTree(Node tree1, Node tree2) {
 		if(tree1 == null && tree2 == null) {
 			return true;
 		}
+		
 		if(tree1 == null || tree2 == null) {
 			return false;
 		}
+		
 		//Check identical case
 		if(quasiIsomorphicBinaryTree(tree1.left,tree2.left) 
 				&& quasiIsomorphicBinaryTree(tree1.right,tree2.right))
 			return true;
+		
 		//Check mirror case
 		if(quasiIsomorphicBinaryTree(tree1.left,tree2.right) 
 				&& quasiIsomorphicBinaryTree(tree1.right,tree2.left))
